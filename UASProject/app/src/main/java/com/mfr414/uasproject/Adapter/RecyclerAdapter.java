b2e3e7ee068fd7314cbb174d9355bffa25d83907
MFR414 <firmansyahm9@gmail.com>
2019-05-27 07:52:22 +0700

    Insert Data To Firebase and read it

--- UASProject/app/src/main/java/com/mfr414/uasproject/Adapter/RecyclerAdapter.java
+++ UASProject/app/src/main/java/com/mfr414/uasproject/Adapter/RecyclerAdapter.java
@@ -3,7 +3,6 @@ package com.mfr414.uasproject.Adapter;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
