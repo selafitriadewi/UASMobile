4426567b8fd631850e20cc151c1a4fac611b4e48
MFR414 <firmansyahm9@gmail.com>
2019-05-27 08:04:37 +0700

    Insert Data To Firebase and read it

---
+++ UASProject/app/src/main/java/com/mfr414/uasproject/Activity/EditJobActivity.java
@@ -0,0 +1,15 @@
+package com.mfr414.uasproject.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mfr414.uasproject.R;
+
+public class EditJobActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_job);
+    }
+}
